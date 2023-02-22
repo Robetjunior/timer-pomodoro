2f9298bd26e1f8b1cafffe8b0794f93927e582cb
Robetjunior <juniorferreirarob@gmail.com>
2023-02-22 12:20:47 -0300

    first steps

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { ThemeProvider } from 'styled-components'
+import { Button } from "./components/Button"
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary"></Button>
+      <Button variant="secondary"></Button>
+      <Button variant="danger"></Button>
+      <Button variant="success"></Button>
+      <Button variant="primary"></Button>
+
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
+export default App
