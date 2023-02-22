a389ef050ea2bca289eadae25a4bc91f86683d55
Robetjunior <juniorferreirarob@gmail.com>
2023-02-22 12:49:31 -0300

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"></Button>
-      <Button variant="secondary"></Button>
-      <Button variant="danger"></Button>
-      <Button variant="success"></Button>
-      <Button variant="primary"></Button>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
       <GlobalStyle />
     </ThemeProvider>
