a6f14e1367c39f20d0f544b733d7660742eea176
Robetjunior <juniorferreirarob@gmail.com>
2023-02-27 23:06:55 -0300

    create reducer and actions

--- src/context/CycleContext.tsx
+++ src/context/CycleContext.tsx
@@ -1,6 +1,12 @@
 import { createContext, ReactNode, useState, useReducer } from 'react'
+import {
+  ActionTypes,
+  addNewCycleAction,
+  interrupetCurrentCycleAction,
+  markCurrentCycleAsFinishedAction,
+} from '../reducers/cycles/actions'
 
-import { ActionTypes, Cycle, cyclesReducer } from '../reducers/cycles'
+import { Cycle, cyclesReducer } from '../reducers/cycles/reducer'
 
 interface CreateCycleData {
   task: string
@@ -38,12 +44,7 @@ export function CyclesContextProvider({
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   function markCurrentCycleAsFinished() {
-    dispatch({
-      type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
-      payload: {
-        activeCycleId,
-      },
-    })
+    dispatch(markCurrentCycleAsFinishedAction())
   }
 
   function setSecondsPassed(seconds: number) {
@@ -61,23 +62,13 @@ export function CyclesContextProvider({
       startDate: new Date(),
     }
 
-    dispatch({
-      type: ActionTypes.ADD_NEW_CYCLE,
-      payload: {
-        newCycle,
-      },
-    })
+    dispatch(addNewCycleAction(newCycle))
 
     setAmountSecondsPassed(0)
   }
 
   function interrupetCurrentCycle() {
-    dispatch({
-      type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-      payload: {
-        activeCycleId,
-      },
-    })
+    dispatch(interrupetCurrentCycleAction())
   }
 
   return (
--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,3 +1,5 @@
+import { ActionTypes } from './actions'
+
 export interface Cycle {
   id: string
   task: string
@@ -13,12 +15,6 @@ interface CyclesState {
   activeCycleId: string | null
 }
 
-export enum ActionTypes {
-  ADD_NEW_CYCLE = 'ADD_NEW_CYCLE',
-  INTERRUPT_CURRENT_CYCLE = 'INTERRUPT_CURRENT_CYCLE',
-  MARK_CURRENT_CYCLE_AS_FINISHED = 'MARK_CURRENT_CYCLE_AS_FINISHED',
-}
-
 export function cyclesReducer(state: CyclesState, action: any) {
   switch (action.type) {
     case ActionTypes.ADD_NEW_CYCLE:
@@ -32,7 +28,7 @@ export function cyclesReducer(state: CyclesState, action: any) {
         ...state,
         cycles: state.cycles.map((cycle) => {
           if (cycle.id === state.activeCycleId) {
-            return { ...cycle, finishedDate: new Date() }
+            return { ...cycle, interruptedDate: new Date() }
           } else {
             return cycle
           }
